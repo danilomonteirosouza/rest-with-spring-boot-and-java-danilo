4c2e4a1e2cddb81400490e7130c3896b9336db57
Danilo Monteiro Souza <danilomonteirosouza1@gmail.com>
2024-09-27 03:57:53 -0300

    initial commit

---
+++ 01_First_Application/rest-with-spring-boot-and-java-danilo/src/test/java/com/awaken/rest_with_spring_boot_and_java_danilo/StartupTests.java
@@ -0,0 +1,13 @@
+package com.awaken.rest_with_spring_boot_and_java_danilo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
